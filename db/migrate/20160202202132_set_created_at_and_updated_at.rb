331422d6902a81fd2d647d55c34f44b2f68ea154
Pawel Niewiadomski <11110000b@gmail.com>
2016-03-26 21:54:07 +0100

    using zena for now, fixed defaults for timestamps

--- db/migrate/20160202202132_set_created_at_and_updated_at.rb
+++ db/migrate/20160202202132_set_created_at_and_updated_at.rb
@@ -1,10 +1,10 @@
 class SetCreatedAtAndUpdatedAt < ActiveRecord::Migration
   def change
     execute <<-SQL
-      ALTER TABLE projects ALTER created_at SET DEFAULT CURRENT_TIMESTAMP;
-      ALTER TABLE projects ALTER updated_at SET DEFAULT CURRENT_TIMESTAMP;
-      ALTER TABLE issues ALTER created_at SET DEFAULT CURRENT_TIMESTAMP;
-      ALTER TABLE issues ALTER updated_at SET DEFAULT CURRENT_TIMESTAMP;
+      ALTER TABLE projects ALTER created_at SET DEFAULT now();
+      ALTER TABLE projects ALTER updated_at SET DEFAULT now();
+      ALTER TABLE issues ALTER created_at SET DEFAULT now();
+      ALTER TABLE issues ALTER updated_at SET DEFAULT now();
     SQL
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214123254) do
+ActiveRecord::Schema.define(version: 20160317202752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pgcrypto"
   enable_extension "uuid-ossp"
 
   create_table "issues", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
